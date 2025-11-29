ea2782c85d7ec96585f9678dda53ce6d60278211
AlpRem <test@me.comm>
2025-11-29 21:42:19 +0300

    split hook.ts on useAppDispatch and useAppSelector

--- src/shared/hooks/hooks.ts
+++
@@ -1,9 +0,0 @@
-import type { AppDispatch, RootState } from '../../app/store';
-import {
-  type TypedUseSelectorHook,
-  useDispatch,
-  useSelector
-} from 'react-redux';
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,1 +1,2 @@
-export { useAppDispatch, useAppSelector } from './hooks';
+export { useAppDispatch } from './useAppDispatch';
+export { useAppSelector } from './useAppSelector';
