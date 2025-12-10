9df9392a70776e04070ba3ab27478d8bee52ea4d
AlpRem <test@me.comm>
2025-12-10 23:14:18 +0300

    add test code from auth (login/logout user)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import styles from './app.module.css';
+import AuthPage from '../pages/AuthPage/AuthPage.tsx';
 
 function App() {
   return (
     <>
       <div className={styles.app}></div>
+      <AuthPage />
     </>
   );
 }
--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -2,9 +2,31 @@ import { useState, type FormEvent } from 'react';
 import styles from './AuthPage.module.css';
 import { AuthForm } from '../../features/auth/ui/AuthForm/AuthForm';
 import { StepIllustration } from '../../shared/ui/StepIllustration';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch } from '../../app/store';
+import {
+  selectAuthError,
+  selectAuthLoading,
+  selectAuthUser,
+  selectIsAuth
+} from '../../app/store/slices/authSlice/authSelector.ts';
+import {
+  fetchLogin,
+  fetchLogout
+} from '../../app/store/slices/authSlice/authSlice.ts';
 
 export const AuthPage = () => {
   const [values, setValues] = useState({ email: '', password: '' });
+
+  // ===== Этот код я добавил =======
+  const isAuth = useSelector(selectIsAuth);
+  const user = useSelector(selectAuthUser);
+
+  const dispatch = useDispatch<AppDispatch>();
+  const isLoading = useSelector(selectAuthLoading);
+  const error = useSelector(selectAuthError);
+
+  // ===== ======= ======= =======
 
   const handleChange = (field: 'email' | 'password', value: string) => {
     setValues((prev) => ({ ...prev, [field]: value }));
@@ -12,7 +34,23 @@ export const AuthPage = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('submit', values);
+
+    // ===== Этот код я добавил =======
+    if (isAuth) {
+      // Пользователь авторизован → разлогиниваем
+      dispatch(fetchLogout())
+        .unwrap()
+        .then(() => console.log('Пользователь вышел'))
+        .catch((err) => console.error('Ошибка выхода:', err));
+    } else {
+      // Пользователь не авторизован → логиним
+      dispatch(fetchLogin(values))
+        .unwrap()
+        .then((user) => console.log('Успешный вход', user))
+        .catch((err) => console.error('Ошибка входа:', err));
+    }
+
+    // ===== ======= ======= =======
   };
 
   return (
@@ -26,7 +64,7 @@ export const AuthPage = () => {
             values={values}
             onChange={handleChange}
             onSubmit={handleSubmit}
-            isLoading={false}
+            isLoading={isLoading}
           />
         </div>
 
