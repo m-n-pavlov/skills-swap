e3f95ab09e766f5b59fda7f363539867cfd10d1d
Gleb Smolyakov <gleb.smolyakov.05@inbox.ru>
2025-12-10 18:47:26 +0200

    рефакторинг skills.js

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import styles from './app.module.css';
+import { Header } from '../widgets/header';
 
 function App() {
   return (
     <>
       <div className={styles.app}></div>
+      <Header isAuth={false}></Header>
     </>
   );
 }
