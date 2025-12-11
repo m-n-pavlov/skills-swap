595bb4c88f85dc5b345f46c58477454bb376ff67
AlpRem <test@me.comm>
2025-12-11 13:43:46 +0300

    delete test row

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
 import styles from './app.module.css';
-import AuthPage from '../pages/AuthPage/AuthPage.tsx';
 
 function App() {
   return (
     <>
       <div className={styles.app}></div>
-      <AuthPage />
     </>
   );
 }
