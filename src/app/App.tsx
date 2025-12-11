e47b95e1e40aa8e3cc5164c73f291ee7978e5dfb
AisuluTurlybai <ais.turlybai@gmail.com>
2025-12-11 12:34:23 +0500

    feat(offer-card): restore app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
 import styles from './app.module.css';
-
-import AuthPage from '../pages/AuthPage/AuthPage';
-import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
 
 function App() {
   return (
     <>
-      <div className={styles.app}>
-        <Routes>
-          <Route path='/login' element={<AuthPage />} />
-          <Route path='/register' element={<RegistrationPage />} />
-        </Routes>
-      </div>
+      <div className={styles.app}></div>
     </>
   );
 }
