1123930a65e297fbd9e11c3a7e06a00ea9ef0a15
Gleb Smolyakov <gleb.smolyakov.05@inbox.ru>
2025-12-11 19:32:22 +0200

    добавлены данные для инициализации

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,20 @@
 import styles from './app.module.css';
+import { useAppDispatch } from '../shared/hooks';
+import { useEffect } from 'react';
+import { fetchGetCategories } from './store/slices/categoriesSlice/categoriesSlice.ts';
+import { fetchGetCities } from './store/slices/citiesSlice/citiesSlice.ts';
+import { fetchGetSkills } from './store/slices/skillsSlice/skillsSlice.ts';
+import { fetchGetUsers } from './store/slices/usersSlice/userSlice.ts';
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchGetCategories());
+    dispatch(fetchGetCities());
+    dispatch(fetchGetSkills());
+    dispatch(fetchGetUsers());
+  }, [dispatch]);
+
   return (
     <>
       <div className={styles.app}></div>
