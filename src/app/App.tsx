ce4ebd9f2f6b9f974e11beaf3b3aeb585dfc9f3a
AlpRem <test@me.comm>
2025-12-11 21:46:27 +0300

    clear app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import styles from './app.module.css';
-import RegistrationPage from '../pages/RegistrationPage/RegistrationPage.tsx';
 
 function App() {
   return (
     <>
-      <RegistrationPage />
       <div className={styles.app}></div>
     </>
   );
