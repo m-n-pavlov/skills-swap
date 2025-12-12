a167081794ce41bf0b1a87cac9f055b59844f1cf
AlpRem <test@me.comm>
2025-12-12 12:19:03 +0300

    add api from auth user

--- src/app/store/slices/authSlice/authSelector.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '../../index.ts';
-
-export const selectAuthUser = (state: RootState) => state.auth.user;
-
-export const selectIsAuth = (state: RootState) => state.auth.isAuth;
