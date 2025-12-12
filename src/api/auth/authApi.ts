73c39d2ca05fa2b398cfef479d93026c04fbd2fd
AlpRem <test@me.comm>
2025-12-12 12:19:03 +0300

    add api from auth user

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -2,7 +2,7 @@ import { checkResponse, type TServerResponse } from '../utils/api.ts';
 import type { TUser } from '../../entities/users.ts';
 
 export type TLoginData = {
-  email: string;
+  login: string;
   password: string;
 };
 
@@ -10,28 +10,34 @@ export type TAuthResponse = TServerResponse<{
   user: TUser;
 }>;
 
-export const loginApi = (data: TLoginData) =>
-  fetch('/api/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(data)
-  }).then((res) => checkResponse<TAuthResponse>(res));
-
-export const logoutApi = (userId?: string) =>
-  fetch('/api/auth/logout', {
+export const loginApi = async (data: TLoginData): Promise<TUser> => {
+  const res = await fetch('/api/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify({
-      userId
+      email: data.login,
+      password: data.password
     })
-  }).then((res) => checkResponse<TServerResponse<{}>>(res));
+  });
+  const json = await checkResponse<TAuthResponse>(res);
+  return json.user;
+};
 
-export const registerApi = (data: FormData) =>
-  fetch('/api/auth/register', {
+export const logoutApi = async (userId?: string): Promise<void> => {
+  const res = await fetch('/api/auth/logout', {
     method: 'POST',
-    body: data
-  }).then((res) => checkResponse<TAuthResponse>(res));
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ userId })
+  });
+  await checkResponse<TServerResponse<{}>>(res);
+};
+
+// export const registerApi = (data: FormData) =>
+//   fetch('/api/auth/register', {
+//     method: 'POST',
+//     body: data
+//   }).then((res) => checkResponse<TAuthResponse>(res));
