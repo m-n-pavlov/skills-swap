74e2ad957ad14ef80d17846dab6f809669c013be
AlpRem <test@me.comm>
2025-12-13 23:55:13 +0300

    add selector from get registrationUser

--- src/app/store/slices/authSlice/authSelector.ts
+++ src/app/store/slices/authSlice/authSelector.ts
@@ -4,6 +4,9 @@ import type { TAuthUser } from '../../../../entities/authUser.ts';
 export const selectCurrentUser = (state: RootState): TAuthUser | null =>
   state.auth.currentUser;
 
+export const selectRegistrationUser = (state: RootState): TAuthUser | null =>
+  state.auth.user;
+
 export const selectIsAuthenticated = (state: RootState): boolean =>
   Boolean(state.auth.currentUser);
 
