6a723dbb60cff87655e66e9c24c506b8e80e6353
Dmitry Chernyshev <black.051@yandex.ru>
2025-12-17 03:29:30 +0500

    feat: создан AuthLayout и MainLayout + кастомный хук useAutoHideFooter

---
+++ src/app/layout/AuthLayout.tsx
@@ -0,0 +1,32 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import type { ReactNode } from 'react';
+import { useAppSelector } from '../../shared/hooks';
+import { AuthHeader } from '../../widgets/AuthHeader';
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useAppSelector((state) => state.auth);
+
+  // Извлекаем "from" из состояния навигации
+  const fromPath = (location.state as { from?: string })?.from || '/';
+
+  const isProtectedRoute = (path: string): boolean => {
+    return path.startsWith('/profile');
+  };
+
+  const handleClose = () => {
+    if (isProtectedRoute(fromPath) && !user) {
+      navigate('/', { replace: true });
+    } else {
+      navigate(fromPath, { replace: true });
+    }
+  };
+
+  return (
+    <>
+      <AuthHeader onClose={handleClose} />
+      <main>{children}</main>
+    </>
+  );
+}
