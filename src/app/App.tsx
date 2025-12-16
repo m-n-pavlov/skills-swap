e30b8262555a82d6521d4de2b41c2e1878b5c8e9
Anastasia Vasiliadi <92613@mail.ru>
2025-12-16 14:12:31 +0300

    Верстка страницы личного кабинета с небольшими правками компонентов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { fetchGetCities } from './store/slices/citiesSlice/citiesSlice.ts';
 import { fetchGetSkills } from './store/slices/skillsSlice/skillsSlice.ts';
 import { fetchGetUsers } from './store/slices/usersSlice/userSlice.ts';
 import HomePage from '../pages/HomePage/HomePage.tsx';
+import { Person } from '../pages/Person/Person.tsx';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -20,6 +21,7 @@ function App() {
   return (
     <>
       <div className={styles.app}>
+        <Person />
         <HomePage />
       </div>
     </>
--- src/pages/Person/Person.tsx
+++ src/pages/Person/Person.tsx
@@ -1,16 +1,36 @@
 import type { FC } from 'react';
 import { PersonForm } from '../../features/PersonForm/ui/PersonForm';
-import style from './Person.module.css';
+import { UserMenu } from '../../widgets/UserMenu';
+
+import styles from './Person.module.css';
+
+const mock = {
+  formValue: {
+    email: '92613@mail.ru',
+    name: 'Anastasia',
+    birthday: new Date('1989-09-26'),
+    gender: 'female' as const,
+    city: 'Москва',
+    description: 'dfghjkl;',
+    password: 'passowrd'
+  }
+};
 
 export const Person: FC = () => {
   return (
-    <section className={style.wrap}>
+    <section className={styles.wrap}>
+      <UserMenu />
       <PersonForm
-        formValue={}
+        /* formValue={}
         handleInputChange={}
         handleInputClick={}
         handleSubmit={}
-        disabled={}
+        disabled={}*/
+        formValue={mock.formValue}
+        handleInputChange={() => console.log('cvbnm,')}
+        handleInputClick={() => console.log('cvbnm,')}
+        handleSubmit={() => console.log('cvbnm,')}
+        disabled={false}
       />
     </section>
   );
