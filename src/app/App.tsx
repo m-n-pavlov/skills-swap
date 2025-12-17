bfc8b8421dfbf3fda757fc420a06b47ec29a3e8b
nat0sh <gvozd.n.y@yandex.kz>
2025-12-17 22:30:51 +0500

    feat: Добавлен роутинг

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import styles from './app.module.css';
-import { useAppDispatch } from '../shared/hooks';
+import { useAppDispatch, useAppSelector } from '../shared/hooks';
 import { useEffect } from 'react';
 import { fetchGetCategories } from './store/slices/categoriesSlice/categoriesSlice.ts';
 import { fetchGetCities } from './store/slices/citiesSlice/citiesSlice.ts';
@@ -9,9 +9,21 @@ import { fetchGetUsers } from './store/slices/usersSlice/userSlice.ts';
 import { SkillPage } from '../pages/SkillPage/SkillPage.tsx';
 import { MainLayout } from './layout/MainLayout.tsx';
 import HomePage from '../pages/HomePage/HomePage.tsx';
+import { Route, Routes } from 'react-router-dom';
+import { PublicRoute } from './router/PublicRoute.tsx';
+import { selectCurrentUser } from './store/slices/authSlice/authSelector.ts';
+import AuthLayout from './layout/AuthLayout.tsx';
+import AuthPage from '../pages/AuthPage/AuthPage.tsx';
+import RegistrationPage from '../pages/RegistrationPage/RegistrationPage.tsx';
+import { ProtectedRoute } from './router/ProtectedRoute.tsx';
+import { FavoritesPage } from '../pages/FavoritesPage/FavoritesPage.tsx';
+import { Person } from '../pages/Person/Person.tsx';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage.tsx';
 
 function App() {
   const dispatch = useAppDispatch();
+  const currentUser = useAppSelector(selectCurrentUser);
+  const isAuth = Boolean(currentUser);
 
   useEffect(() => {
     dispatch(fetchGetCategories());
@@ -21,13 +33,85 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <div className={styles.app}>
-        <MainLayout>
-          <HomePage />
-        </MainLayout>
-      </div>
-    </>
+    <div className={styles.app}>
+      <Routes>
+        {/* Главная */}
+        <Route
+          path='/'
+          element={
+            <MainLayout>
+              <HomePage />
+            </MainLayout>
+          }
+        />
+
+        {/* Авторизация */}
+        <Route
+          path='/login'
+          element={
+            <PublicRoute isAuthenticated={isAuth} restricted>
+              <AuthLayout>
+                <AuthPage />
+              </AuthLayout>
+            </PublicRoute>
+          }
+        />
+        <Route
+          path='/registration'
+          element={
+            <PublicRoute isAuthenticated={isAuth} restricted>
+              <AuthLayout>
+                <RegistrationPage />
+              </AuthLayout>
+            </PublicRoute>
+          }
+        />
+
+        {/* Избранное */}
+        <Route
+          path='/favorites'
+          element={
+            <ProtectedRoute isAuthenticated={isAuth}>
+              <MainLayout>
+                <FavoritesPage />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Профиль */}
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute isAuthenticated={isAuth}>
+              <MainLayout>
+                <Person />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Навык */}
+        <Route
+          path='/skill'
+          element={
+            <MainLayout>
+              <SkillPage />
+            </MainLayout>
+          }
+        />
+
+        {/* 404 */}
+        <Route
+          path='*'
+          element={
+            <MainLayout>
+              <NotFoundPage />
+            </MainLayout>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
--- src/widgets/Header/type.ts
+++ src/widgets/Header/type.ts
@@ -1,6 +1,5 @@
 import type { TCategory } from '../../entities/categories.ts';
 import type { TAuthUser } from '../../entities/authUser.ts';
-import type { ChangeEvent } from 'react';
 
 export interface HeaderProps {
   isAuth: boolean;
